969358ea29245241393be57fd246a44f4d6a89f2
Macau <mau_santos09@hotmail.com>
2018-06-04 00:49:20 -0300

    add graphql, rootQuery and swagger

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const hapi = require('hapi');
 const mongoose = require('mongoose');
 const Painting = require('./models/Painting');
+const { graphqlHapi, graphiqlHapi } = require('apollo-server-hapi');
+const schema = require('./graphql/schema');
+
+const Inert = require('inert');
+const Vision = require('vision');
+const HapiSwagger = require('hapi-swagger');
+const Pack = require('./package');
+
 
 const server = hapi.server({
-    port: 4001,
-    host: 'localhost'
+	port: 4000,
+	host: 'localhost'
 });
 
 mongoose.connect('mongodb://test:test@ds235860.mlab.com:35860/macau-powerful-api');
@@ -14,17 +22,56 @@ mongoose.connection.once('open', () => {
 });
 
 const init = async () => {
+
+	await server.register([
+		Inert,
+		Vision,
+		{
+			plugin: HapiSwagger,
+			options: {
+				info: {
+					title: 'Paintings API Documentation',
+					version: Pack.version
+				}
+			}
+		}
+	]);
+
+	await server.register({
+		plugin: graphiqlHapi,
+		options: {
+			path: '/graphiql',
+			graphiqlOptions: {
+				endpointURL: '/graphql'
+			},
+			route: {
+				cors: true
+			}
+		}
+	});
+
+	await server.register({
+		plugin: graphqlHapi,
+		options: {
+			path: '/graphql',
+			graphqlOptions: {
+				schema
+			},
+			route: {
+				cors: true
+			}
+		}
+	});
+
+
 	server.route([
 		{
 			method: 'GET',
-			path: '/',
-			handler: function(request, reply) {
-				return `<h1>My modern api</h1>`;
-			}
-		},
-		{
-			method: 'GET',
 			path: '/api/v1/paintings',
+			config: {
+				description: "Get a specific painting by Id.",
+				tags: ['api', 'v1', 'paintings']
+			},
 			handler: (req, reply) => {
 				return Painting.find();
 			}
@@ -32,6 +79,10 @@ const init = async () => {
 		{
 			method: 'POST',
 			path: '/api/v1/paintings',
+			config: {
+				description: "Add a new painting.",
+				tags: ['api', 'v1', 'paintings']
+			},
 			handler: (req, reply) => {
 				const { name, url, techniques } = req.payload;
 				const painting = new Painting({
